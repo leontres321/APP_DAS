56c2c79e05981c57e037557746eaa4e86008df5b
Leonardo <leonardo.sobarzo@sansano.usm.cl>
2017-06-12 15:53:54 -0300

    Add files via upload

---
+++ android/build/intermediates/assets/debug/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="score" tilewidth="32" tileheight="32" tilecount="132" columns="11">
+ <image source="score.png" trans="000000" width="361" height="401"/>
+</tileset>
